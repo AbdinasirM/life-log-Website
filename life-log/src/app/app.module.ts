566ed56ac88f6ac771acae2e74a9b0a5e361961c
Abdi <aabdi5520@gmail.com>
2023-08-09 08:35:02 -0500

    Added testimonial and copy right section.

--- life-log/src/app/app.module.ts
+++ life-log/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignInAndSignUpComponent } from './sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SignInAndSignUpComponent,
-    SignInComponent
+    SignInComponent,
+    FeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
